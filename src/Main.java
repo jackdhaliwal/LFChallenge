2005570ffa5ba3360334e69c92889259ce688d7f
Brinder Dhaliwal <brinder.dhaliwal13@gmail.com>
2020-02-08 01:19:54 -0800

    Code Cleanup and final commenting for submission

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// Runner for project
+		// Runner for project.
 		
 		
-		// Take user selection
+		// Take user selection.
 		Scanner in = new Scanner(System.in);
 
-		// Array of possible menu choices 
+		// Possible menu choices.
 		String[] arr = {"simpleSort Ascending", "simpleSort Descending", "printSortedFrequency", "numberOfUniqueValues"};
-		for( int i = 0; i <= arr.length-1; i++)
-
+		for( int i = 0; i <= arr.length-1; i++) {
               System.out.println(i+1 + ": " + arr[i]);
-
-        System.out.println("q. Quit");
-
+		}
+		
+        System.out.println("q. Quit Program");
+        System.out.println(" ");
 
         boolean quit = false;
 
         String menuItem;
 
         do {
-
               System.out.print("Choose menu item: ");
 
               menuItem = in.nextLine();
@@ -33,93 +32,112 @@ public class Main {
               switch (menuItem) {
 
               case "1":
-
                     System.out.println("simpleSort Ascending: ");
                     System.out.println(" ");
-
-                    List<Integer> someAscendingNums = new ArrayList<Integer>();
-                    someAscendingNums.add(24);
-                    someAscendingNums.add(12);
-                    someAscendingNums.add(8);
-                    someAscendingNums.add(12);
-                    someAscendingNums.add(19);
-            		System.out.println("Supplied List: " + someAscendingNums);
+                    
+                    // Create supplied list and add example elements.
+                    List<Integer> ascendingNumList = new ArrayList<Integer>();
+                    ascendingNumList.add(24);
+                    ascendingNumList.add(12);
+                    ascendingNumList.add(8);
+                    ascendingNumList.add(12);
+                    ascendingNumList.add(19);
+                    
+            		System.out.println("Supplied List: " + ascendingNumList);
+            		
+            		// Create instance of Sorter class that implements the interface.
             		Sorter sortListAscending = new Sorter();
-            		System.out.println("Ascending List: " + sortListAscending.simpleSort(someAscendingNums));
-
+            		
+            		// Call to simpleSort method.
+            		System.out.println("Ascending List after sorting: " + sortListAscending.simpleSort(ascendingNumList));
                     break;
               
               case "2":
-
-                  System.out.println("simpleSort Descending: ");
-                  System.out.println(" ");
-
-                  List<Integer> someDescendingNums = new ArrayList<Integer>();
-                someDescendingNums.add(24);
-                someDescendingNums.add(12);
-                someDescendingNums.add(8);
-                someDescendingNums.add(12);
-                someDescendingNums.add(19);
-          		System.out.println("Supplied List: " + someDescendingNums);
-          		Sorter sortListDescending = new Sorter();
-          		System.out.println("Descending List: " + sortListDescending.simpleSortDescendingHelper(someDescendingNums));
-
-                  break;
-              
+                  	System.out.println("simpleSort Descending: ");
+                  	System.out.println(" ");
+                
+                  	// Create supplied list and add example elements.
+                  	List<Integer> descendingNumList = new ArrayList<Integer>();
+                  	descendingNumList.add(24);
+                  	descendingNumList.add(12);
+                  	descendingNumList.add(8);
+                  	descendingNumList.add(12);
+                  	descendingNumList.add(19);
+                
+                  	System.out.println("Supplied List: " + descendingNumList);
+          		
+                  	// Create new instance of Sorter class that implements the interface.
+                  	Sorter sortListDescending = new Sorter();
+                  	
+                  	// Call to simpleSort method (descending).
+                  	System.out.println("Descending List after sorting: " + sortListDescending.simpleSortDescendingHelper(descendingNumList));
+                  	break;
+                  	
+                  	// Take a list and print them in ascending order to standard out along with number of times 
+                  	// that the respective number appears.
              case "3":
 
-            	 System.out.println("printSortedFrequency: ");
-                 System.out.println(" ");
-
-                List<Integer> someNumsTobePassed = new ArrayList<Integer>();
-                someNumsTobePassed.add(24);
-                someNumsTobePassed.add(12);
-                someNumsTobePassed.add(8);
-                someNumsTobePassed.add(12);
-                someNumsTobePassed.add(19);
-          		System.out.println("Supplied List: " + someNumsTobePassed);
-          		Sorter printFreq = new Sorter();
-          		printFreq.printSortedFrequency(someNumsTobePassed);
-
-                  break;
-                  
+            	 	System.out.println("printSortedFrequency: ");
+            	 	System.out.println(" ");
+            	 	
+            	 	// Create supplied list and add example elements.
+            	 	List<Integer> sortedListToBePrinted = new ArrayList<Integer>();
+            	 	sortedListToBePrinted.add(24);
+            	 	sortedListToBePrinted.add(12);
+            	 	sortedListToBePrinted.add(8);
+            	 	sortedListToBePrinted.add(12);
+            	 	sortedListToBePrinted.add(19);
+            	 	
+            	 	System.out.println("Supplied List: " + sortedListToBePrinted);
+            	 	
+            	 	// Create instance of Sorter class that implements the interface.
+            	 	Sorter frequencyToStandardOut = new Sorter();
+            	 	frequencyToStandardOut.printSortedFrequency(sortedListToBePrinted);
+            	 	break;
+            	 	
+            	 	// Take a list and return number of times a unique value appears .
             case "4":
 
-            	 System.out.println("numberOfUniqueValues: ");
-                 System.out.println(" ");
-
-                List<Integer> someNumOfUniques = new ArrayList<Integer>();
-                someNumOfUniques.add(24);
-                someNumOfUniques.add(12);
-                someNumOfUniques.add(8);
-                someNumOfUniques.add(12);
-                someNumOfUniques.add(19);
-          		System.out.println("Supplied List: " + someNumOfUniques);
-          		Sorter uniqueItems = new Sorter();
-          		System.out.println("Unique values: " + uniqueItems.numberOfUniqueValues(someNumOfUniques));
-
-                  break;
-
-
-     
+            	 	System.out.println("numberOfUniqueValues: ");
+            	 	System.out.println(" ");
+            	 	
+            	 	// Create supplied list and add example elements.
+            	 	List<Integer> listOfUniqueValues = new ArrayList<Integer>();
+            	 	listOfUniqueValues.add(24);
+            	 	listOfUniqueValues.add(12);
+            	 	listOfUniqueValues.add(8);
+            	 	listOfUniqueValues.add(12);
+            	 	listOfUniqueValues.add(19);
+            	 	
+            	 	// Additional test case
+            	 	
+            	 	//listOfUniqueValues.add(24);
+  
+            	 	
+            	 	System.out.println("Supplied List: " + listOfUniqueValues);
+            	 	
+            	 	// Create instance of Sorter class that implements the interface.
+            	 	Sorter uniqueItems = new Sorter();
+            	 	System.out.println("Unique values: " + uniqueItems.numberOfUniqueValues(listOfUniqueValues));
+            	 	break;
+            	 	
+            	 	// Quit Program.
               case "q":
-
+            	  	
                     quit = true;
-
                     break;
-
+                    
+                    // User didn't select a valid menu choice.
               default:
-
                     System.out.println("Invalid choice.");
-
               }
-
-        } while (!quit);
-
-        System.out.println("Bye-bye!");
-
-  }
-
+              		// If the user doesn't quit program, keep looping menu.
+        	} while (!quit);
+        			
+        			// When program exits, print a friendly goodbye message.
+        	System.out.println("Thank you, Goodbye!");
 	}
+
+}
 
 
--- src/Sorter.java
+++ src/Sorter.java
@@ -4,76 +4,106 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.HashSet;
 
-public class Sorter implements DifferentListSorting {
+public class Sorter implements ListSorting {
 	
+	// Method to sort list of numbers in ascending order. 
 	@Override
-	public List<Integer> simpleSort(List<Integer> a) {
-		for (int i = 0; i<a.size(); i++) {
-			for(int j =a.size() - 1; j > i; j--) {
-				if (a.get(i) > a.get(j)) {
-					int temp = a.get(i);
-					a.set(i, a.get(j));
-					a.set(j, temp);
+	public List<Integer> simpleSort(List<Integer> listOfNumsToBeSortedAsc) {
+		
+		// "i" is a pointer for the outer loop.
+		for (int i = 0; i < listOfNumsToBeSortedAsc.size(); i++) {
+			
+			// "j" is a pointer for the inner loop.
+			for(int j = listOfNumsToBeSortedAsc.size() - 1; j > i; j--) {
+				
+				// If value at index of i is greater than value at index of j,
+				// then perform a swap.
+				if (listOfNumsToBeSortedAsc.get(i) > listOfNumsToBeSortedAsc.get(j)) {
+					
+					// Swapping 
+					int temp = listOfNumsToBeSortedAsc.get(i);
+					listOfNumsToBeSortedAsc.set(i, listOfNumsToBeSortedAsc.get(j));
+					listOfNumsToBeSortedAsc.set(j, temp);
 				}
 			}
 		}	
-		return a;
+		return listOfNumsToBeSortedAsc;
 	}
-
+	
+	// Method to print sorted (ascending) list and number of times each element appears. 
 	@Override
-	public void printSortedFrequency(List<Integer> z) {
+	public void printSortedFrequency(List<Integer> listTobeSortedAndPrinted) {
 		
-		// a map to hold elements (keys) and respective counts (values)
-		// using a TreeMap because we care about the order once we print to output
+		// A map to hold elements (keys) and respective "number of times they appear" counts (values).
+		// Using a TreeMap because we care about the order once we print to output.
 		Map<Integer, Integer> mapOfNumsAndValues = new TreeMap<Integer, Integer>();
 
-		for (int num : z) {
+		for (int num : listTobeSortedAndPrinted) {
+			
+			// If an element is already in the map, increase the number of times count by 1
 		    if (mapOfNumsAndValues.containsKey(num)) {
-		    	// if element appears more than once in list
 		    	mapOfNumsAndValues.put(num, mapOfNumsAndValues.get(num) + 1);
 		        
 		    } else {
-		    	// if element doesn't appear more than once in list
+		    	// If the element is not already in the map, simply add it and assign "1" to the number of times count
 		    	mapOfNumsAndValues.put(num, 1);
 		    }
 		}
 		
-		
-		// Using Map.Entry which returns a collection-view of all mappings
-		// (we can iterate over key/values)
+		// We can modify our map to be able to iterate and retrieve key/values using Map.Entry.
+		// For every record in our map, print the element and respective number of times it appears.
 		for (Map.Entry<Integer, Integer> record : mapOfNumsAndValues.entrySet()) {
 		    System.out.println(record.getKey() + " appears " + record.getValue() + " time(s)");
 		}
 	}
-
+	
+	// Method to return the number of times a unique value appears.
 	@Override
-	public int numberOfUniqueValues(List<Integer> c) {
+	public int numberOfUniqueValues(List<Integer> listOfPotentialUniqueNums) {
+		
+		// Using a set to hold elements as opposed to a map as sets do not have duplicate elements.
 		Set<Integer> setOfNums = new HashSet<Integer>();
-		int unique=0;
-		for (int num : c) {
+		
+		// A counter for unique elements.
+		int uniqueCount=0;
+		
+		// Add each element from the list to the set.
+		for (int num : listOfPotentialUniqueNums) {
 		    if (!setOfNums.contains(num)) {
 		    	setOfNums.add(num);  
 		    }
 		}
-		for (int i : setOfNums) {
-			if (setOfNums.contains(i)) {
-				unique++;
+		
+		// Iterate over our set and if there are unique elements, increase our unique counter.
+		for (int uniqueNum : setOfNums) {
+			if (setOfNums.contains(uniqueNum)) {
+				uniqueCount++;
 			}
 		}    
-		return unique;
+		return uniqueCount;
 	}
 	
-	public List<Integer> simpleSortDescendingHelper(List<Integer> b) {
-		for (int i = 0; i<b.size(); i++) {
-			for(int j =b.size() - 1; j > i; j--) {
-				if (b.get(i) < b.get(j)) {
-					int temp = b.get(i);
-					b.set(i, b.get(j));
-					b.set(j, temp);
+	// Method to sort list of numbers in descending order. 
+	public List<Integer> simpleSortDescendingHelper(List<Integer> listOfNumsToBeSortedDesc) {
+		
+		// "i" is a pointer for the outer loop.
+		for (int i = 0; i<listOfNumsToBeSortedDesc.size(); i++) {
+			
+			// "j" is a pointer for the inner loop.
+			for(int j = listOfNumsToBeSortedDesc.size() - 1; j > i; j--) {
+				
+				// If value at index of i is less than value at index of j,
+				// then perform a swap.
+				if (listOfNumsToBeSortedDesc.get(i) < listOfNumsToBeSortedDesc.get(j)) {
+					
+					// Swapping
+					int temp = listOfNumsToBeSortedDesc.get(i);
+					listOfNumsToBeSortedDesc.set(i, listOfNumsToBeSortedDesc.get(j));
+					listOfNumsToBeSortedDesc.set(j, temp);
 				}
 			}
 		}	
-		return b;
+		return listOfNumsToBeSortedDesc;
 		
 	}
 	
